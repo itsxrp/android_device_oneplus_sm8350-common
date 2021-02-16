7bd76f5e224a110f83d33dde3ca239fb941e92aa
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sm8350-common: Migrate to libperfmg power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- common.mk
+++ common.mk
@@ -245,12 +245,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
-$(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
+    android.hardware.power-service.lineage-libperfmgr
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
@@ -284,6 +279,9 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
     hardware/pixelworks/interfaces
 
